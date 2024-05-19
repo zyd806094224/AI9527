5c263f0340b12118b96ee5ccd48619d8d7038f65
zhaoyudong <806094224@qq.com>
2024-05-19 22:24:02 +0800

    测试WebSocket

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,10 +1,33 @@
 // index.js
 Page({
 
-    testClick(){
+    testClick() {
         console.log('点击了')
+        wx.setStorageSync('username', 'test')
+        wx.connectSocket({
+            url: 'ws://192.168.1.8:80/chatWebSocket/' + wx.getStorageSync('username')
+        })
+        wx.onSocketOpen((res) => {
+            // socketOpen = true
+            console.log("打开socket");
+            wx.showToast({
+                icon: 'none',
+                title: '会话建立成功',
+                duration: 500
+            })
+            // socketMsgQueue = []
+            wx.onSocketMessage((result) => {
+                result.data = result.data.replace(" ", "&nbsp;");
+                console.log(result.data);
+            })
+        })
+
     },
-    onPullDownRefresh(){
+    onPullDownRefresh() {
         console.log('=======')
+    },
+
+    onShow() {
+
     }
 })
